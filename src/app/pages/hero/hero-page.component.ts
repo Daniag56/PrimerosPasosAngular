db4dea62f486964e9de2d00dd907b56dfc2ccd0a
daniag56 <dnlrsgrc@gmail.com>
2026-09-07 18:31:48 +0200

    Señales computadas y Pipes

--- src/app/pages/hero/hero-page.component.ts
+++ src/app/pages/hero/hero-page.component.ts
@@ -1,15 +1,24 @@
 /*Para usar señales siempre debemos importarlas*/
-import { Component, signal } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
   selector: 'app-hero-page',
   standalone: true,
   templateUrl: './hero-page.component.html',
+  imports: [UpperCasePipe]
 })
 export class HeroPageComponent {
 
   name = signal('IronMan');
   age = signal(45);
+
+  heroDescription = computed(() => {   //Las señales computadas son de angular core, necesitan ser importadas
+    const description = `${ this.name()} - ${this.age()}`;
+    return description;
+  })
+
+  capitalizedName = computed(() => this.name().toUpperCase());
 
   getHeroDescription(){
     return `${'hero: '+this.name()} - ${'age: '+this.age()}`;
